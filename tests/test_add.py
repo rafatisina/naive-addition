43b9d8f74492a010f5de134672e096a36e9255a5
Sina Rafati <33768396+rafatisina@users.noreply.github.com>
2022-05-04 22:45:11 -0500

    tests added

--- tests/test_add.py
+++ tests/test_add.py
@@ -5,3 +5,8 @@ def test_add():
 def test_add_string():
     assert add("Fourth","Brain")=="FourthBrain"
 
+def test_add_1array():
+    assert add([1,2]) == 103
+	
+def test_add_2array():
+    assert add ([1,2],[3,4])==10
